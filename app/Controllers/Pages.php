90f38326338f78c70a69851f2916fb77588e997c
jaenudin0901 <jaenudinj913198@gmail.com>
2020-07-06 22:02:07 +0700

    inital commit

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Pandai Ngoding'
+        ];
+
+        return view('pages/home', $data);
+    }
+    public function about()
+    {
+        $data = [
+            'title' => 'About Me'
+        ];
+
+        return view('pages/about', $data);
+    }
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+
+                    'tipe' => 'Rumah',
+                    'alamat' => 'Jl taman patra V',
+                    'kota' => 'Jakarta Selatan'
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. I gede anak agung ',
+                    'kota' => 'Jakarta selatan'
+                ]
+
+            ]
+        ];
+
+        return view('pages/contact', $data);
+    }
+
+
+    //--------------------------------------------------------------------
+
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, facilis sed odio architecto, asperiores sequi accusantium qui aliquid, iure fugiat cupiditate illum at ratione exercitationem? Consequuntur laboriosam soluta sint rem?</p>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
